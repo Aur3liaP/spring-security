c43f59af9e0cf09e84ab898bf46799b76b3b4652
Pic Aurélia <pic.aurelia@gmail.com>
2025-06-10 14:13:44 +0200

    TP02-JWT p2 correction

--- src/main/java/fr/diginamic/spring_security/service/JwtAuthentificationService.java
+++ src/main/java/fr/diginamic/spring_security/service/JwtAuthentificationService.java
@@ -1,5 +1,6 @@
 package fr.diginamic.spring_security.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtAuthentificationService {
     private final String secret =  "monSecretLeMieuxGardeDeTouteMaVieEntiereQuePersonneNeDoitSavoir";
+
+    public Boolean validateJwt(String jwtToken) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .build()
+                    .parseClaimsJws(jwtToken)
+                    .getBody();
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
 
     public String generateToken() {
         return Jwts.builder()
